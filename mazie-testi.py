4bbf9e7f86521528b26c90cf1bd6b0c164300c73
Aleksandr Zelukin <92846541+AleksandrZelukin@users.noreply.github.com>
2025-01-24 20:12:43 +0200

    Update apps

--- mazie-testi.py
+++ mazie-testi.py
@@ -3,29 +3,29 @@ import lietotaji
 import mekletajs
 import lasitava
 
-# gr1 = iespieddarbi.Gramata("Zvaigzne", "Pēteris", "Zveja", "Lielā kaķu sugu grāmata", 2010, 2, 110)
-# print(gr1)
-# print(gr1.__class__.__name__)
-# gr1.saglabat_db()
-#
-# zhurn = iespieddarbi.Periodika("Dienas žurnāli","Dadzis", "2020", "1", 3, 11)
-# print(zhurn)
-# zhurn.saglabat_db()
+gr1 = iespieddarbi.Gramata("Zvaigzne", "Pēteris", "Zveja", "Lielā kaķu sugu grāmata", 2010, 2, 110)
+print(gr1)
+print(gr1.__class__.__name__)
+gr1.saglabat_db()
 
-# darbinieks = lietotaji.Darbinieks("Jānis", "Grāmatnieks", "112233-44556", "29000000", "2000-01-01", "secret")
-# lietotaji.saglabat_DB(darbinieks)
+zhurn = iespieddarbi.Periodika("Dienas žurnāli","Dadzis", "2020", "1", 3, 11)
+print(zhurn)
+zhurn.saglabat_db()
 
-# lasitajs = lietotaji.Lasitajs("Linda", "Grāmatniece", "121212-22222", "83000000", "2021-03-30")
-# lietotaji.saglabat_DB(lasitajs)
+darbinieks = lietotaji.Darbinieks("Jānis", "Grāmatnieks", "112233-44556", "29000000", "2000-01-01", "secret")
+lietotaji.saglabat_db(darbinieks)
 
-# mekletajs.meklet_gramatu("Pēteris", "Zve", "kaķu")
-# print(mekletajs.meklet_periodiku("Dadzis", "", ""))
-# print(mekletajs.meklet_lasitaju("Linda", "", ""))
-# print(mekletajs.lasitaja_dati("2465f4b9-6276-4309-9c5b-a69a7b1ede54"))
+lasitajs = lietotaji.Lasitajs("Linda", "Grāmatniece", "121212-22222", "83000000", "2021-03-30")
+lietotaji.saglabat_db(lasitajs)
 
-# print(lasitava.aprekinat_kavejuma_naudu(2))
+mekletajs.meklet_gramatu("Pēteris", "Zve", "kaķu")
+print(mekletajs.meklet_periodiku("Dadzis", "", ""))
+print(mekletajs.meklet_lasitaju("Linda", "", ""))
+print(mekletajs.lasitaja_dati("2465f4b9-6276-4309-9c5b-a69a7b1ede54"))
 
-# print(lietotaji.parbaude("123123-12312", "programmesana"))
+print(lasitava.aprekinat_kavejuma_naudu(2))
 
-# mekletajs.rezultatu_tabula([6, 9])
-# print(mekletajs.izdevuma_info(7))
+print(lietotaji.parbaude("123123-12312", "programmesana"))
+
+mekletajs.rezultatu_tabula([6, 9])
+print(mekletajs.izdevuma_info(7))
